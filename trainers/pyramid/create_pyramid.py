9ab0370fe7f93c68a4b2dbf7ec8b179c56402b32
Anton Zhulanov <zhulanoff2007@gmail.com>
2021-10-09 14:03:15 +0300

    переделал формирование пирамиды

--- trainers/pyramid/create_pyramid.py
+++ trainers/pyramid/create_pyramid.py
@@ -2,21 +2,17 @@ from PIL import Image, ImageDraw, ImageFont
 import random
 
 
-def created_img(W:int, H:int):
+def created_img(W: int, H: int):
     """Тут содаётся белый фон для пирамиды"""
     img = Image.new('RGBA', (W, H), 'white')
     return img
 
 
-def sorted_words():
+def get_words(len_=4):
     """Тут мы отсеиваем подходящие слова из файла"""
-    words = []
-    with open('trainers/pyramid/word_rus.txt', 'r') as file:
-        for line in file:
-            if len(line.strip()) == 4:
-                words.append(line.strip())
-    return words
-    
+    with open('trainers/pyramid/word_rus.txt', 'r', encoding='utf-8') as file:
+        return [str.strip(word) for word in file.readlines() if len(str.strip(word)) == len_]
+
 
 def format_word(number):
     """Из колекции слов выбираем случайные слова и форматируем их для пирамиды,
@@ -24,7 +20,10 @@ def format_word(number):
     num = 2
     formated_text = []
     # Выбираем случайные слова пять штук
-    words = random.sample(sorted_words(), number)
+    words = get_words()
+    random.shuffle(words)
+    words = words[:number]
+    # words = random.sample(sorted_words(), number)
     for word in words:
         space = " " * num
         formated_text.append(f"{word[:2]}{space}*{space}{word[2:]}")
@@ -32,29 +31,29 @@ def format_word(number):
     return formated_text
 
 
-def create_pyramid(number:int=5):
+def create_pyramid(height):
     """Строим из полученых слов пирамиду"""
     # Получаем размер изображения на котором будет строится пирамида
-    W = 100 + 65 * number
-    H = 22 * number + 40
-    #Создаём новое изображение 
+    W = 100 + 65 * height
+    H = 22 * height + 40
+    # Создаём новое изображение
     img = created_img(W, H)
     # Чтобы пирамида была ровной используем свободный моноширный шрифт
     font = ImageFont.truetype('font/Fira_Code/static/FiraCode-Regular.ttf', 18)
     # Переменая для отсупов между словами по высоте
     space = 0
-    text = format_word(number)
+    text = format_word(height)
     for word in text:
         # Получаем размер слова
-        w,h = font.getsize(word)
-        #добавляем отступ
+        w, h = font.getsize(word)
+        # добавляем отступ
         space = space + 25
         idraw = ImageDraw.Draw(img)
         idraw.text(
-            ((W-w)/2, space - (h/2) ),
+            ((W - w) / 2, space - (h / 2)),
             word,
             font=font,
             fill=('#000000'),
-            )
-    #Сохраняем получившуюся картинку
+        )
+    # Сохраняем получившуюся картинку
     return img.save('images/pyramid.png')
